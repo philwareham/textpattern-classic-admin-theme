a8ffb1093bf825cf2d7ad5cdd5888e147dca5948
Phil Wareham <phil@designhive.com>
2014-04-09 17:21:00 +0100

    Start moving stuff around for new build

---
+++ Gruntfile.js
@@ -0,0 +1,114 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        compass: {
+            dev: {
+                options: {
+                    config: 'config.rb',
+                    force: true
+                }
+            }
+        },
+
+        copy: {
+            dist: {
+                files: [
+                    {expand: true, cwd: 'src/', src: ['*'], dest: 'dist/classic/', filter: 'isFile'},
+                    {expand: true, cwd: 'src/assets/img/', src: ['**'], dest: 'dist/classic/assets/img/'}
+                ]
+            }
+        },
+
+        cssmin: {
+            main: {
+                expand: true,
+                cwd: 'dist/classic/assets/css/',
+                src: ['*.css', '!*.min.css'],
+                dest: 'dist/classic/assets/css/',
+                ext: '.min.css'
+            }
+        },
+
+        jshint: {
+            files: ['Gruntfile.js', 'src/assets/js/*.js'],
+            options: {
+                bitwise: true,
+                camelcase: true,
+                curly: true,
+                eqeqeq: true,
+                es3: true,
+                forin: true,
+                immed: true,
+                indent: 4,
+                latedef: true,
+                noarg: true,
+                noempty: true,
+                nonew: true,
+                quotmark: 'single',
+                undef: true,
+                unused: true,
+                strict: true,
+                trailing: true,
+                browser: true,
+                globals: {
+                    jQuery: true,
+                    module: true,
+                    responsiveNav: true,
+                    prettyPrint: true
+                }
+            }
+        },
+
+        uglify: {
+            dist: {
+                options: {
+                    mangle: false,
+                    preserveComments: 'some'
+                },
+
+                files: [
+                    {
+                        'dist/classic/assets/js/main.js': ['src/assets/js/main.js'],
+                        'dist/classic/assets/js/responsivenav.js': ['bower_components/responsive-nav/responsive-nav.js'],
+                        'docs/assets/js/prettify/prettify.js': ['bower_components/google-code-prettify/src/prettify.js']
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/google-code-prettify/src/',
+                        src: 'lang-*.js',
+                        dest: 'docs/assets/js/prettify/'
+                    }
+                ]
+            }
+        },
+
+        watch: {
+            sass: {
+                files: 'src/assets/sass/**',
+                tasks: ['sass']
+            },
+
+            js: {
+                files: 'src/assets/js/*.js',
+                tasks: ['jshint', 'copy', 'uglify']
+            }
+        }
+
+    });
+
+    grunt.registerTask('build', ['jshint', 'sass', 'copy', 'uglify']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('sass', ['compass', 'cssmin']);
+    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('travis', ['jshint', 'compass']);
+};
