040af74312c64b54bb7eeda389a90ab7f6957cec
Phil Wareham <phil@designhive.com>
2014-10-26 20:06:10 +0000

    Docs: latest amends to docs, mirrored from Hive theme

--- docs/assets/js/design-patterns.js
+++ docs/assets/js/design-patterns.js
@@ -1,3 +1,8 @@
+/*!
+ * This example file is used to demonstrate all available jQuery UI components
+ * on the design patterns page.
+ */
+
 $(function ()
 {
     /**
@@ -12,20 +17,32 @@ $(function ()
      * jQuery UI interactions.
      */
 
-    // Draggable
+    /**
+     * Draggable
+     */
+
     $('.jquery-ui-draggable').draggable({
         cursor: 'move'
     });
 
-    // Resizable
+    /**
+     * Resizable.
+     */
+
     $('.jquery-ui-resizable').resizable({
         minWidth: 200
     });
 
-    // Selectable
+    /**
+     * Selectable.
+     */
+
     $('.jquery-ui-selectable').selectable();
 
-    // Sortable
+    /**
+     * Sortable.
+     */
+
     $('.jquery-ui-sortable').sortable({
         placeholder: 'ui-sortable-placeholder',
         cursor: 'ns-resize'
@@ -35,24 +52,52 @@ $(function ()
      * jQuery UI widgets.
      */
 
-    // Accordion
+    /**
+     * Accordion.
+     */
+
     $('.jquery-ui-accordion').accordion({
         header: 'h3'
     });
 
-    // Autocomplete
+    /**
+     * Accordion RTL.
+     *
+     * Flip positioning of submenus when in RTL langauges.
+     */
+
+    $('[dir="rtl"] .jquery-ui-accordion').accordion({
+        icons: {
+            'header': 'ui-icon-triangle-1-w',
+            'activeHeader': 'ui-icon-triangle-1-s'
+        }
+    });
+
+    /**
+     * Autocomplete.
+     */
+
     var countryList = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo, Democratic Republic', 'Congo, Republic of the', 'Costa Rica', 'Cote d’Ivoire', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Greenland', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, North', 'Korea, South', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Mongolia', 'Morocco', 'Monaco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Samoa', 'San Marino', ' Sao Tome', 'Saudi Arabia', 'Senegal', 'Serbia and Montenegro', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe'];
     $('#autocomplete-country').autocomplete({
         source: countryList
     });
 
-    // Button
+    /**
+     * Button.
+     */
+
     $('.jquery-ui-button').button();
 
-    // Button set
+    /**
+     * Button set.
+     */
+
     $('.jquery-ui-buttonset').buttonset();
 
-    // Icon button
+    /**
+     * Icon button.
+     */
+
     $('.jquery-ui-button-icon-only').button({
         icons: {
             primary: 'ui-icon-locked'
@@ -83,7 +128,10 @@ $(function ()
         text: false
     });
 
-    // Split button
+    /**
+     * Split button.
+     */
+
     $('#split-rerun-example')
         .button()
         .click(function ()
@@ -116,53 +164,64 @@ $(function ()
         .hide()
         .menu();
 
-    // Split button search example
-    $('.txp-search-button')
-        .button({
-            text: false,
-            icons: {
-                primary: 'ui-icon-search'
-            }
-        })
-        .click(function ()
-        {
-            alert('Running the last action');
-        })
-        .next()
-        .button({
-            text: false,
-            icons: {
-                primary: 'ui-icon-triangle-1-s'
-            }
-        })
-        .click(function ()
-        {
-            var menu = $(this).parent().next().show().position({
-                my: 'left top',
-                at: 'left bottom',
-                of: this
-            });
-            $(document).one('click', function ()
-            {
-                menu.hide();
-            });
-            return false;
-        })
-        .parent()
-        .buttonset()
-        .next()
-        .hide()
-        .menu();
+    /**
+     * Split button search example.
+     */
 
-    // Options button
-    $('#detail-toggle').button({
+    var search = $('.txp-search');
+
+    search.find('.txp-search-button').button({
+        text: false,
+        icons: {
+            primary: 'ui-icon-search'
+        }
+    })
+    .click(function ()
+    {
+        alert('Running the last action');
+    });
+
+    search.find('.txp-search-options').button({
+        text: false,
+        icons: {
+            primary: 'ui-icon-triangle-1-s'
+        }
+    })
+    .click(function (e)
+    {
+        var menu = search.find('.txp-dropdown').toggle().position({
+            my: 'right top',
+            at: 'right bottom',
+            of: this
+        });
+        $(document).one('click blur', function ()
+        {
+            menu.hide();
+        });
+        return false;
+    });
+
+    search.find('.txp-search-buttons').buttonset();
+
+    search.find('.txp-dropdown').hide().menu().click(function(e) {
+        e.stopPropagation();
+    });
+
+    /**
+     * Options button.
+     */
+
+    $('.txp-button-details').button({
         text: false,
         icons: {
             primary: 'ui-icon-gear'
         }
     });
 
-    // List view button
+    /**
+     * List view button.
+     */
+
     $('.txp-button-list').button({
         text: false,
         icons: {
@@ -170,7 +229,10 @@ $(function ()
         }
     });
 
-    // Grid view button
+    /**
+     * Grid view button.
+     */
+
     $('.txp-button-grid').button({
         text: false,
         icons: {
@@ -178,22 +240,34 @@ $(function ()
         }
     });
 
-    // Datepicker
+    /**
+     * Datepicker.
+     */
+
     $('.jquery-ui-datepicker').datepicker().children().show();
 
-    // Dialog (basic)
+    /**
+     * Dialog (basic).
+     */
+
     $('#jquery-ui-dialog').dialog({
         autoOpen: false
     });
 
-    // Dialog (modal) link
+    /**
+     * Dialog (modal) link.
+     */
+
     $('#jquery-ui-dialog-link').click(function ()
     {
         $('#jquery-ui-dialog').dialog('open');
         return false;
     });
 
-    // Dialog (modal)
+    /**
+     * Dialog (modal).
+     */
+
     $('#jquery-ui-dialog-modal').dialog({
         autoOpen: false,
         modal: true,
@@ -210,22 +284,54 @@ $(function ()
         }
     });
 
-    // Dialog (modal) link
+    /**
+     * Dialog (modal) link.
+     */
+
     $('#jquery-ui-dialog-modal-link').click(function ()
     {
         $('#jquery-ui-dialog-modal').dialog('open');
         return false;
     });
 
-    // Menu
-    $('.jquery-ui-menu').menu();
+    /**
+     * Menu.
+     */
 
-    // Progressbar
+    $('.jquery-ui-menu').menu({
+        position: {
+            at: 'right top-1'
+        }
+    });
+
+    /**
+     * Menu RTL.
+     *
+     * Flip positioning of submenus when in RTL langauges.
+     */
+
+    $('[dir="rtl"] .jquery-ui-menu').menu({
+        position: {
+            my: 'right top',
+            at: 'left top-1'
+        },
+        icons: {
+            submenu: 'ui-icon-carat-1-w'
+        }
+    });
+
+    /**
+     * Progressbar.
+     */
+
     $('.jquery-ui-progressbar').progressbar({
         value: 37
     });
 
-    // Progressbar (indeterminate)
+    /**
+     * Progressbar (indeterminate).
+     */
+
     $('.jquery-ui-progressbar2').progressbar({
         value: false
     });
@@ -239,16 +345,38 @@ $(function ()
         event.preventDefault();
     });
 
-    // Selectmenu
+    /**
+     * Selectmenu.
+     */
+
     $('.jquery-ui-selectmenu').selectmenu();
 
-    // Slider - horizontal
+    /**
+     * Selectmenu RTL.
+     *
+     * Flip positioning of selectmenu dropdowns when in RTL langauges.
+     */
+
+    $('[dir="rtl"] .jquery-ui-selectmenu').selectmenu({
+        position: {
+            my: 'left top',
+            at: 'right bottom'
+        }
+    });
+
+    /**
+     * Slider - horizontal.
+     */
+
     $('.jquery-ui-slider-horizontal').slider({
         range: true,
         values: [17, 67]
     });
 
-    // Slider - vertical
+    /**
+     * Slider - vertical.
+     */
+
     $('.jquery-ui-slider-vertical > span').each(function ()
     {
         var value = parseInt($(this).text());
@@ -260,7 +388,10 @@ $(function ()
         });
     });
 
-    // Spinner
+    /**
+     * Spinner.
+     */
+
     $('#jquery-ui-spinner').spinner({
         spin: function (event, ui)
         {
@@ -277,10 +408,16 @@ $(function ()
         }
     });
 
-    // Tabs
+    /**
+     * Tabs.
+     */
+
     $('.jquery-ui-tabs').tabs();
 
-    // Tooltips
+    /**
+     * Tooltips.
+     */
+
     $('.jquery-ui-tooltip').tooltip();
 
 });
