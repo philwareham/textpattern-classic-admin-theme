b71a4c8b34410e819c4825bc2af0374c6cfd6f56
Phil Wareham <phil@designhive.com>
2017-11-23 16:49:30 +0000

    Remove multiselect JS test

--- docs/assets/js/design-patterns.js
+++ docs/assets/js/design-patterns.js
@@ -284,7 +284,7 @@ $(function ()
     })
     .click(function ()
     {
-        alert('Running the last action');
+        alert('Running the search action');
     });
 
     search.find('.txp-search-options').button({
@@ -384,79 +384,5 @@ $(function ()
     $('.txp-textfilter-options-list').hide().menu().click(function(e) {
         e.stopPropagation();
     });
-
-
-
-    // Split button search example NEW.
-
-    var search = $('.txp-search-new');
-
-    search.find('.txp-search-button').button({
-        showLabel: false,
-        icon: 'ui-icon-search'
-    })
-    .click(function ()
-    {
-        alert('Running the last action');
-    });
-
-    //search.find('.txp-search-options').button({
-    //    showLabel: false,
-    //    icon: 'ui-icon-triangle-1-s'
-    //})
-    //.click(function (e)
-    //{
-    //    if (langdir === 'rtl') {
-    //        var menu = search.find('.txp-dropdown').toggle().position({
-    //            my: 'left top',
-    //            at: 'left bottom',
-    //            of: this
-    //        });
-    //    } else {
-    //        var menu = search.find('.txp-dropdown').toggle().position({
-    //            my: 'right top',
-    //            at: 'right bottom',
-    //            of: this
-    //        });
-    //    };
-    //
-    //    $(document).one('click blur', function ()
-    //    {
-    //        menu.hide();
-    //    });
-    //
-    //    return false;
-    //});
-
-    //search.find('.txp-search-buttons').controlgroup();
-
-    search.find('.txp-dropdown').hide().menu().click(function(e) {
-        e.stopPropagation();
-    });
-
-    // Multiselect plugin testing.
-
-    if (langdir === 'rtl') {
-        $(".txp-search-dropdown-new").multiselect({
-            header: false,
-            height: 'auto',
-            position: {
-                my: 'left top',
-                at: 'left bottom',
-                of: this
-           }
-        });
-    } else {
-        $(".txp-search-dropdown-new").multiselect({
-            header: false,
-            height: 'auto',
-            position: {
-                my: 'right top',
-                at: 'right bottom',
-                of: this
-           }
-        });
-    };
-
 
 });
