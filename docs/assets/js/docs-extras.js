e21854d28eae0f4fdb7d05b156685229b9cf5cdd
philwareham <phil@designhive.com>
2020-06-16 13:03:09 +0100

    Start 4.9 structural changes
    Buld will be broken temporarily

---
+++ docs/assets/js/docs-extras.js
@@ -0,0 +1,17 @@
+(function()
+{
+    'use strict';
+
+    // ScrollSpy.
+
+    var spy = new ScrollSpy('main', {
+        nav: '.design-patterns-menu ol > li > a',
+        className: 'is-inview'
+    });
+
+    var subSpy = new ScrollSpy('main', {
+    	nav: '.design-patterns-menu ol > li > ol > li > a',
+    	className: 'is-inview'
+    });
+
+})();
